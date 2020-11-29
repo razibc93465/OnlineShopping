8ec5ef0a73e9f931a87e7a9df89451829a1757d5
razibc93465 <razibc93465@gmail.com>
2020-11-29 17:14:30 +0600

    Add README.md

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use View;
+use App\Category;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //       View::share('name', 'BITM');
+//        View::composer('frontEnd.includes.menu', function($view){
+//             $view->with('name', 'BITM');
+        View::share('name', 'BITM');
+        View::composer('frontEnd.includes.menu', function($view) {
+            $publishedCategories = Category::where('publicationStatus', 1)->get();
+            $view->with('publishedCategories', $publishedCategories);
+        });
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
